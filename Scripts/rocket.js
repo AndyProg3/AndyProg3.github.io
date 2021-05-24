b41c7af57c15b8df22cf8062758ba9ea9fec19ee
AndyProg3 <78244781+AndyProg3@users.noreply.github.com>
2021-05-24 09:55:40 -0400

    Animations added to index header

--- Scripts/rocket.js
+++ Scripts/rocket.js
@@ -1,12 +1,29 @@
 
 $(document).ready(function() {
 
-    $("#Rocket").click(function() {
+    $("#rocket").click(function() {
         $(this).css("animation", "blastoff 1.1s");
 
         window.setTimeout(function () {
             window.location.href = "CPlusProjects.html#Captured";
         }, 1000);
     });
+
+
+    lightSwitch(0);
+
+    function lightSwitch(num) {
+        if($("#l" + num).length != 0) {
+            $("#l" + num).css("color", "white");
+
+            window.setTimeout(function () {
+                lightSwitch(parseInt(num) + 1);
+            }, 150);
+        }
+        else {
+            $(".fade").css("animation", "fadein 3.0s");
+            $(".fade").css("opacity", 1);
+        }
+    }
 
 });
